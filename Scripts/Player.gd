fea0059d014ca2cf05b796de3dfc4331fd133926
SolangeLL <solange.le-luel@epitech.eu>
2023-03-25 21:56:11 +0100

    :construction: Add jumping gravity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,15 +3,18 @@ extends KinematicBody2D
 var run_speed = 350
 var jump_speed = -300
 var gravity = 1700
+
 var velocity = Vector2()
+var jumping = false
 
 func get_input():
 	velocity.x = 0
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_pressed("ui_select")
+	var jump = Input.is_action_just_pressed("ui_select")
 	
-	if jump:
+	if jump and is_on_floor():
+		jumping = true
 		velocity.y = jump_speed
 		gravity *= -1
 		jump_speed *= -1
@@ -24,8 +27,9 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	print(is_on_floor())
 	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, 1))
 	pass
