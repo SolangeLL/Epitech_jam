bcda69146a5a7844fb5e77caff45540c9f14af76
SolangeLL <solange.le-luel@epitech.eu>
2023-03-26 12:29:26 +0200

    :fire: Erase double jumps

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,16 +10,14 @@ func get_input():
 	velocity.x = 0
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed("jump")
+	var jump = Input.is_action_pressed("jump")
 	var revert = Input.is_action_just_pressed("revert")
-	
-	if jump and is_on_floor():
+
+	if jump and (is_on_floor() or is_on_ceiling()):
 		velocity.y = jump_speed
 	if revert:
 		gravity *= -1
 		jump_speed *= -1
-		$GravityDirection.target_position.y *= -1
-		print(Vector3.UP)
 	if right:
 		velocity.x += run_speed
 	if left:
@@ -31,9 +29,9 @@ func _ready():
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	get_input()
-	var direction = Input.get_axis("ui_left", "ui_right")
-	velocity.x = direction * run_speed
 	move_and_slide()
+	
+	# Rotate sprite according to ground normal
 	var normal = get_floor_normal()
 	$Sprite2D.rotation = normal.angle()
 	pass
