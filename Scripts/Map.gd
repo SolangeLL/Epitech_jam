5f64ee9d88f1babaccabdd65cbc84126b8cfd4b1
SolangeLL <solange.le-luel@epitech.eu>
2023-03-26 21:50:46 +0200

    :construction: Blocks for map

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -9,17 +9,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$UpFloor.global_position = $UpFloor.global_position.move_toward($UpFloor/Marker2D.global_position, delta * speed)
-	$DownFloor.global_position = $DownFloor.global_position.move_toward($UpFloor/Marker2D.global_position, delta * speed)
+	$UpFloor/FirstBlock.global_position = $UpFloor/FirstBlock.global_position.move_toward($UpFloor/Marker2D.global_position, delta * speed)
+	$UpFloor/SecondBlock.global_position = $UpFloor/SecondBlock.global_position.move_toward($UpFloor/Marker2D.global_position, delta * speed)
+	$DownFloor/FirstBlock.global_position = $DownFloor/FirstBlock.global_position.move_toward($UpFloor/Marker2D.global_position, delta * speed)
+	$DownFloor/SecondBlock.global_position = $DownFloor/SecondBlock.global_position.move_toward($UpFloor/Marker2D.global_position, delta * speed)
 	pass
 
 
 
 func up_block_exited():
-	$UpFloor/FirstBlock.global_position.x = get_viewport().get_visible_rect().size.x
+	print("respon first")
+	$UpFloor/FirstBlock.global_position.x = get_viewport().get_visible_rect().size.x * 2
 	pass # Replace with function body.
 
 
 func up_second_block_exited():
+	print("respon second")	
 	$UpFloor/SecondBlock.global_position.x = get_viewport().get_visible_rect().size.x	
 	pass # Replace with function body.
