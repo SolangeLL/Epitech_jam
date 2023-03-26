69380898f58584aade086c042a1a8bcf14e70bec
Blisteur <nathan.chiodin@gmail.com>
2023-03-26 15:21:25 +0200

    add obstacle

---
+++ Scripts/Obstacle_move.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Turret_body_entered(body):
+	if body.is_in_group("Player"):
+		body.die()
+
+func auto_move(delta):
+	global_position = global_position.move_toward($Marker2D.global_position, delta)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	auto_move(delta)
